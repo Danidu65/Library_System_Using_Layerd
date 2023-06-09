993c86605ac03ac54d807005e45029525eddcd53
Danidu Viraj <daniduijse65@Gmail.com>
2023-06-09 22:19:38 +0530

    commit

---
+++ Library_System/src/model/AuthorDTO.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+    private String id;
+    private String Name;
+    private String BookName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getBookName() {
+        return BookName;
+    }
+
+    public void setBookName(String bookName) {
+        BookName = bookName;
+    }
+}
