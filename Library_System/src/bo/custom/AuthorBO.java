9ca7814072fd32efb4d004441cfa1124569c9701
Danidu Viraj <daniduijse65@Gmail.com>
2023-06-13 11:54:04 +0530

    commit

--- Library_System/src/bo/custom/AuthorBO.java
+++ Library_System/src/bo/custom/AuthorBO.java
@@ -1,4 +1,28 @@
 package bo.custom;
 
+import dto.AuthorDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface AuthorBO {
+    ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException;
+
+    ArrayList<CustomerDTO> getAllAuthors() throws SQLException, ClassNotFoundException;
+
+    boolean addCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException;
+
+    String generateNewCustomerID() throws SQLException, ClassNotFoundException;
+
+    boolean updateAuthor(CustomerDTO dto) throws SQLException, ClassNotFoundException;
+
+    boolean addAuthor(AuthorDTO dto) throws SQLException, ClassNotFoundException;
+
+    boolean updateAuthor(AuthorDTO dto) throws SQLException, ClassNotFoundException;
+
+    boolean searchAuthor(String id) throws SQLException, ClassNotFoundException;
+
+    boolean deleteAuthor(String id) throws SQLException, ClassNotFoundException;
+
+    String generateNewAuthorID() throws SQLException, ClassNotFoundException;
 }
--- Library_System/src/bo/custom/impl/AuthorBOImpl.java
+++ Library_System/src/bo/custom/impl/AuthorBOImpl.java
@@ -1,4 +1,49 @@
 package bo.custom.impl;
 
-public class AuthorBOImpl {
+import bo.custom.AuthorBO;
+import dao.DAOFactory;
+import dao.custom.AuthorDAO;
+import dto.AuthorDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class AuthorBOImpl implements AuthorBO {
+
+    AuthorDAO authorDAO = (AuthorDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.AUTHOR);
+
+    @Override
+    public ArrayList<AuthorDTO> getAllAuthors() throws SQLException, ClassNotFoundException {
+        ArrayList<AuthorDTO> allCustomers= new ArrayList<>();
+        ArrayList<Author> all = authorDAO.getAll();
+        for (Author c : all) {
+            allCustomers.add(new AuthorDTO(c.get(),c.getName(),c.getAddress()));
+        }
+        return allCustomers;
+    }
+
+    @Override
+    public boolean addAuthor(AuthorDTO dto) throws SQLException, ClassNotFoundException {
+        return AuthorDAO.add(new Author(dto.getId(), dto.getName(), dto.getAddress()));
+    }
+
+    @Override
+    public boolean updateAuthor(AuthorDTO dto) throws SQLException, ClassNotFoundException {
+        return authorDAO.update(new Author(dto.getId(),dto.getName(),dto.getAddress()));
+    }
+
+    @Override
+    public boolean searchAuthor(String id) throws SQLException, ClassNotFoundException {
+        return authorDAO.search(id);
+    }
+
+    @Override
+    public boolean deleteAuthor(String id) throws SQLException, ClassNotFoundException {
+        return authorDAO.delete(id);
+    }
+
+    @Override
+    public String generateNewAuthorID() throws SQLException, ClassNotFoundException {
+        return authorDAO.generateNewID();
+    }
 }
