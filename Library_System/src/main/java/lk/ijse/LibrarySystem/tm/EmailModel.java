d9e224b8b965f234f4959a13c9e67d73d6cd3acc
Danidu Viraj <daniduijse65@Gmail.com>
2023-06-18 21:29:31 +0530

    commit

--- Library_System/src/main/java/lk/ijse/LibrarySystem/tm/EmailModel.java
+++ Library_System/src/main/java/lk/ijse/LibrarySystem/tm/EmailModel.java
@@ -1,7 +1,6 @@
-package lk.ijse.LibrarySystem.Model;
+package lk.ijse.LibrarySystem.tm;
 
 import javax.mail.*;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
