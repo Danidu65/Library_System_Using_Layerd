ddb1b19821d05946efda1b9a71aa74c8f1a9ffc3
Danidu Viraj <daniduijse65@Gmail.com>
2023-06-23 13:20:14 +0530

    commit

--- Library_System/src/main/java/lk/ijse/LibrarySystem/dao/custom/ReturnDAO.java
+++ Library_System/src/main/java/lk/ijse/LibrarySystem/dao/custom/ReturnDAO.java
@@ -5,5 +5,6 @@ import lk.ijse.LibrarySystem.entity.Return;
 
 public interface ReturnDAO extends CrudDAO<Return> {
     static boolean ReturnSet(Return aReturn, String qty, String bookId, String issuseID) {
+        return false;
     }
 }
