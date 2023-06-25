f939df9c298eabe93f958cfbd4a5ea4d97f97239
Danidu Viraj <daniduijse65@Gmail.com>
2023-06-25 20:35:01 +0530

    commit

--- Library_System/src/main/java/lk/ijse/LibrarySystem/bo/custom/UserBO.java
+++ Library_System/src/main/java/lk/ijse/LibrarySystem/bo/custom/UserBO.java
@@ -4,7 +4,6 @@ import lk.ijse.LibrarySystem.dto.UserDTO;
 import lk.ijse.LibrarySystem.entity.User;
 
 import java.sql.SQLException;
-
 public interface UserBO extends SuplierBO{
     public boolean userAdd(UserDTO user) throws SQLException, ClassNotFoundException;
     public User usersearchFrom(String id) throws SQLException, ClassNotFoundException;
