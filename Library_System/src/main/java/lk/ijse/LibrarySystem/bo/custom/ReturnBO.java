7a864eaa6150fce91e218b2d027c989e75b6962c
Danidu Viraj <daniduijse65@Gmail.com>
2023-06-28 22:53:54 +0530

    commit

--- Library_System/src/main/java/lk/ijse/LibrarySystem/bo/custom/ReturnBO.java
+++ Library_System/src/main/java/lk/ijse/LibrarySystem/bo/custom/ReturnBO.java
@@ -5,6 +5,7 @@ import lk.ijse.LibrarySystem.dto.ReturnDTO;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 public interface ReturnBO extends SuperBO {
     public  boolean ReturnSet(ReturnDTO returns, String Qty, String BookId, String IssuseID) throws SQLException;
     public ArrayList<ReturnDTO> loadAll() throws SQLException, ClassNotFoundException;
