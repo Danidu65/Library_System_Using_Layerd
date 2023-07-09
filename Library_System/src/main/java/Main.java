59b9d3cabaa707acfece786e3586321a074fa0c0
Danidu Viraj <daniduijse65@Gmail.com>
2023-07-09 23:58:48 +0530

    commit

--- Library_System/src/main/java/Main.java
+++ Library_System/src/main/java/Main.java
@@ -3,6 +3,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
